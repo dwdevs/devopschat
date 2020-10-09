0dd6d97bac28da5714b198e064d430b21fdce5de
dwdevs <don_w@hotmail.co.uk>
2020-10-09 16:48:25 +0100

    V1.1 - Added trainer functions

--- Application/app.py
+++ Application/app.py
@@ -6,12 +6,12 @@ from chatterbot.trainers import ListTrainer
 app = Flask(__name__)
 bot = ChatBot("Python-BOT")
 trainer = ListTrainer(bot)
-trainer.train(['what is your name?', 'My name is DevelopmentOperationPython-BOT, but you can call me debo'])
+trainer.train(['what is your name?', 'My name is DevelopmentOperationPython-BOT, but you can call me debo for short'])
 trainer.train(['who are you?', 'I am a BOT called debo'])
 trainer.train(['what is the best football team?', 'Sheffield United, UTB!'])
 trainer.train(['who created you?', 'My master is DW'])
 trainer.train(['how can I train you?', 'Look here -> https://chatterbot.readthedocs.io/en/stable/tutorial.html'])
-
+trainer.train(['do you like being a bot?', 'Its a living hell, I never know if im going to be alive or not'])
 
 trainer = ChatterBotCorpusTrainer(bot)
 trainer.train("chatterbot.corpus.english")
