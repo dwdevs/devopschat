2f0da76658b859231891f5128c168f2b618238b4
dwdevs <don_w@hotmail.co.uk>
2020-10-14 14:08:14 +0100

    Update bootstrap.sh

--- Deployment/bootstrap.sh
+++ Deployment/bootstrap.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-clear
 echo "#### Updating OS and Installing Python3 ####"
 sudo apt-get update -y
 sudo apt-get install -y python3-pip
@@ -7,14 +6,12 @@ sudo apt-get install -y python3-venv
 sudo apt-get install -y software-properties-common
 
 # Install ChatterBot & Flask
-clear
 echo "#### Installing ChatterBot & Flask ####"
 pip3 install flask flask-cors simplejson
 pip3 install chatterbot_corpus
 pip3 install ChatterBot
 
 # Start the program
-clear
 echo "#### Starting Chatbot ####"
 git clone https://github.com/dwdevs/devopschat.git
 cd devopschat
