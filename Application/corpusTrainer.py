bd3baaebeab373698699ac59d1a08d319d30cd08
dwdevs <don_w@hotmail.co.uk>
2020-10-19 16:28:14 +0100

    Update corpusTrainer.py

--- Application/corpusTrainer.py
+++ Application/corpusTrainer.py
@@ -5,14 +5,5 @@ from chatterbot.trainers import ChatterBotCorpusTrainer
 trainer = ChatterBotCorpusTrainer(bot)
 trainer.train("chatterbot.corpus.english")
 
-# Training with Personal Ques & Ans 
-#training_data_simple = open('training_data/normal.txt').read().splitlines()
-#training_data_personal = open('training_data/all.txt').read().splitlines()
-
-#training_data = training_data_simple + training_data_personal
-
-#trainer = ListTrainer(chatbot)
-#trainer.train(training_data)
-
 # Training with English Corpus Data 
 trainer_corpus = ChatterBotCorpusTrainer(chatbot)
